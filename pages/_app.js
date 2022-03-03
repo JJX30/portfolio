fb04fb7dd96dbe6c2dd802405247b103a919c6f3
Mauricio Del Castillo <mauriciodelcas30@gmail.com>
2022-03-02 20:42:53 -0500

    about, contact, work

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/contact.js
@@ -0,0 +1,5 @@
+function Contact() {
+  return <div>contract page</div>;
+}
+
+export default Contact;
