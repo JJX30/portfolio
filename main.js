41c6c53f5fb75a4e89a1e71122b7092f552c96dc
Mauricio Del Castillo <mauriciodelcas30@gmail.com>
2022-03-14 19:17:37 -0400

    home page

---
+++ main.js
@@ -0,0 +1,77 @@
+import "./style.css";
+
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const scene = new THREE.Scene();
+
+const spaceTexture = new THREE.TextureLoader().load("space-texture.jpeg");
+scene.background = spaceTexture;
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const earthTexture = new THREE.TextureLoader().load("earth-texture.png");
+
+const torusKnot = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: earthTexture,
+  })
+);
+
+scene.add(torusKnot);
+
+const pointLight = new THREE.PointLight(0xbdc8ff);
+pointLight.position.set(5, 1, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+
+scene.add(lightHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+Array(200).fill().forEach(addStar);
+
+function animate() {
+  requestAnimationFrame(animate);
+  // torusKnot.rotation.x += 0.01;
+  torusKnot.rotation.y += 0.0005;
+  // torusKnot.rotation.z += 0.01;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,8 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head></Head>
+
       <Component {...pageProps} />
     </div>
   );
