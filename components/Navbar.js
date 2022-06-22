8c00568ba5e19d9d5d16776884b9dc165b576b96
Mauricio Del Castillo <mauriciodelcas30@gmail.com>
2022-06-22 17:15:47 -0400

    hamburger navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import styles from "../styles/navbar.module.css";
-import React, { useState, useEffect } from "react";
-import GiHamburgerMenu from "react-icons";
+import React, { useState, useEffect, useRef } from "react";
+import { CgMenu } from "react-icons/cg";
 
 const Navbar = () => {
+  const menu = useRef();
   const [small, setSmall] = useState(false);
   const [width, setWidth] = useState(0); // default width, detect on server.
   useEffect(() => {
@@ -26,10 +27,40 @@ const Navbar = () => {
     };
   }, [width]);
 
+  const handleClick = () => {
+    if (menu.current.style.visibility == "visible") {
+      menu.current.style.visibility = "hidden";
+    } else {
+      menu.current.style.visibility = "visible";
+    }
+  };
+
   return (
     <div>
       {small ? (
-        <div></div>
+        <div className={styles.navbar}>
+          <div className={styles.navbar_left}>
+            <div className={styles.navbar_title_small}>
+              <Link href="/">Mauricio Del Castillo</Link>
+            </div>
+          </div>
+          <div className={styles.navbar_right}>
+            <div onClick={handleClick} className={styles.navbar_hamburger}>
+              <CgMenu className={styles.icon} size={47}></CgMenu>
+              <div ref={menu} className={styles.navbar_hamburger_hidden}>
+                <div className={styles.navbar_content_hidden}>
+                  <Link href="/work">work</Link>
+                </div>
+                <div className={styles.navbar_content_hidden}>
+                  <Link href="/about">about</Link>
+                </div>
+                <div className={styles.navbar_content_hidden}>
+                  <Link href="/contact">contact</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       ) : (
         <div className={styles.navbar}>
           <div className={styles.navbar_left}>
